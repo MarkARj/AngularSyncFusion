53dffc82acef158492e9c7cb03a21605429256c5
MarkARj <elia.chayban@net.usj.edu.lb>
2019-09-24 11:51:50 +0200

    SyncFusion Integration Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ejs-grid [datasource]="data"></ejs-grid>',
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-grid-app';
-}
+  public data: object[]=[
+    {
+      ProdID:1,
+      ProductName:"AAA",
+      Price:"$123",
+      Production:"China",
+      Manufacturer:"CCC Ltd",
+      Discount:"20%"
+    },
+    {
+      ProdID:2,
+      ProductName:"ABA",
+      Price:"$103",
+      Production:"Japan",
+      Manufacturer:"CCD Ltd",
+      Discount:"10%"
+    },
+    {
+      ProdID:3,
+      ProductName:"CBA",
+      Price:"$1203",
+      Production:"China",
+      Manufacturer:"CCX Ltd",
+      Discount:"15%"
+    }
+  ]
+  }
